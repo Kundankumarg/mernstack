d3e28477efae9327457f13dea371c872b7e048c7
kundan gupta <kundankumar712712712@gmail.com>
2026-04-09 22:14:52 +0530

    learn if-else

---
+++ condition.js
@@ -0,0 +1,23 @@
+let weather = 'Rainy';
+
+if(weather==='Rainy')
+{
+    console.log("umberella ley kar aoo");
+}
+else{
+
+}
+
+
+if(a===2)
+{
+    console.log()
+}
+else if(b===2)
+{
+
+}
+else if(c===3)
+{
+
+}
