395d7cdf624465ef0031d81a53cd86e87c94169b
kundan gupta <kundankumar712712712@gmail.com>
2026-04-09 14:25:40 +0530

    Day one starting learning javascript function

---
+++ hello.js
@@ -0,0 +1,23 @@
+console.log("hii")
+
+function printchai()
+{
+    console.log("chai");
+}
+
+function bringBrush(n)
+{
+    console.log(`Hanji ley aye ${n} brush`)
+}
+
+function add(a,b)
+{
+    console.log(`Sum of ${a} + ${b} = `,a+b);
+}
+
+
+
+
+printchai();
+bringBrush(5);
+add(3,4);
