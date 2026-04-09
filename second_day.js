2a8b9278d018d346f0d159a9fabd23f5b9806195
kundan gupta <kundankumar712712712@gmail.com>
2026-04-09 21:45:59 +0530

    Today i learn Data types

---
+++ second_day.js
@@ -0,0 +1,47 @@
+// Primitive Data Types
+
+
+// string -> "",'',``
+
+let name = "Rohit";
+
+// Number -> 25, 25.9
+
+let age = 25;
+
+//  Boolean -> true,false
+
+let ispaid = true;
+
+// null
+let favouriteclass = null;
+
+// undefined
+
+let hometown;
+console.log(hometown)
+
+// symbol
+
+
+// BigInt
+
+
+
+
+
+// non Primitive Data Types
+
+// array
+let skills = ["html","css", "javascript"]
+
+// Object
+let studentprofile = {
+    name:"kundan",
+    age:23,
+    ispaid:true,
+    skills : ["html","css", "javascript"],
+    favouriteclass :null
+}
+
+
