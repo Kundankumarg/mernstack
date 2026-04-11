3b1014c720b08a0f467291bb6deff8ad423882ab
kundan gupta <kundankumar712712712@gmail.com>
2026-04-11 09:50:20 +0530

    loop in javascript

---
+++ loop.js
@@ -0,0 +1,8 @@
+let teas = ["masala", "ginger" , "oolong", "orange" , "rose" , "Lemon"]
+
+console.log(teas[0]);
+
+for(let i = 0; i<teas.length; i++)
+{
+    console.log(`tea at index ${i}:${teas[i]}`)
+}
