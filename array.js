f064cebaf7bf55a0a1bf79620150e7f801384f43
kundan gupta <kundankumar712712712@gmail.com>
2026-04-11 09:49:15 +0530

    learn array

---
+++ array.js
@@ -0,0 +1,17 @@
+let fruits = ["apple","cherry","banana"]
+console.log(fruits)
+console.log(typeof(fruits))
+
+
+let newfruits = new Array("grapes","kiwi","pineapple")
+console.log(newfruits)
+console.log(typeof(newfruits))
+
+
+fruits.push(1) // push in the last
+console.log(fruits)
+
+fruits.unshift("papaya") // add in the first element of the array
+console.log(fruits)
+
+fruits.pop() // Delete from the last element
