cd33317922ceeedbbec6ca1c40e6ef39c11706ac
kundan gupta <kundankumar712712712@gmail.com>
2026-04-13 10:34:59 +0530

    array practice

---
+++ practice/array.js
@@ -0,0 +1,27 @@
+//Creating an array using array literal involves using square brackets [] to define and initialize the array.
+
+// Creating an Empty Array
+let a =[];
+console.log(a);
+
+// Creating an Array and Initializing with Values
+let b =[1,"c",3,4];
+console.log(b);
+
+//The "Array Constructor" refers to a method of creating arrays by invoking the Array constructor function.
+
+let c = new Array(10,20,30);
+console.log(c);
+
+// Accessing Elements of an Array
+let arr = ["HTML","CSS","java","php"];
+console.log(arr[0]);
+
+// Accessing the Last Element of an Array
+let last = ["HTML","CSS","java","php"];
+let final = last[last.length - 1];
+console.log(`last element is : `,final);
+
+
+
+
