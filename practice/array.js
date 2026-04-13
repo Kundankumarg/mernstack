d145e6b1a3703ae218f4265f976514928b75cc57
kundan gupta <kundankumar712712712@gmail.com>
2026-04-13 10:40:33 +0530

    Modifying the Array Elements

--- practice/array.js
+++ practice/array.js
@@ -9,7 +9,6 @@ let b =[1,"c",3,4];
 console.log(b);
 
 //The "Array Constructor" refers to a method of creating arrays by invoking the Array constructor function.
-
 let c = new Array(10,20,30);
 console.log(c);
 
@@ -21,6 +20,11 @@ console.log(arr[0]);
 let last = ["HTML","CSS","java","php"];
 let final = last[last.length - 1];
 console.log(`last element is : `,final);
+
+//  Modifying the Array Elements
+let m = ["HTML","CSS","java","php"];
+m[1]="javascript";
+console.log(m);
 
 
 
